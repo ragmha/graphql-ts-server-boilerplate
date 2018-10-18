3fc0f61dc8ad0eab68512ceaca90bd18c13b3b44
Raghib Hasan <raghibmidhat@gmail.com>
2018-10-18 05:43:37 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import { importSchema } from 'graphql-import';
+import { GraphQLServer } from 'graphql-yoga';
+import { resolvers } from './resolvers';
+import { createConnection } from 'typeorm';
+
+const typeDefs = importSchema('schema.graphql');
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+createConnection().then(() => {
+  server.start(() => console.log('Server is running on localhost:4000'));
+});
